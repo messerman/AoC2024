3e4f7d595df6b068938600a3bb212ccffe0ff348
Matt Messerman <mmesserman@netflix.com>
2024-12-11 16:35:04 -0800

    11.02 solved

--- 11/solution.py
+++ 11/solution.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import cProfile
+from math import ceil, log
 
-PARTS = [1]#, 2]
-FILES = ['sample.txt', 'input.txt']
+PARTS = [2]#[1, 2]
+FILES = ['input.txt'] #['sample.txt', 'input.txt']
 PAUSE = False
 
 class Stone:
@@ -15,23 +16,35 @@ class Stone:
     def __str__(self) -> str:
         return str(self.value)
 
-    def blink(self) -> list['Stone']:
+    CACHE: dict[(tuple[int, int]), int] = {}
+
+    def blink(self, depth=0, maxdepth=75) -> int:
+        if maxdepth < 1:
+            self.CACHE[(self.value, depth)] = 1
+            return self.CACHE[(self.value, depth)]
+
+        if (self.value, depth) in self.CACHE:
+            return self.CACHE[(self.value, depth)]
+
         # If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
         if self.value == 0:
-            self.value = 1
-            return [self]
-        
+            stone = Stone(1)
+            self.CACHE[(self.value, depth)] = stone.blink(depth+1, maxdepth-1)
+            return self.CACHE[(self.value, depth)]           
+
         # If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
         strval = str(self.value)
         strlen = len(strval)
         if strlen % 2 == 0:
-            self.value = int(strval[:strlen//2])
+            left  = Stone(int(strval[:strlen//2]))
             right = Stone(int(strval[strlen//2:]))
-            return [self, right]
+            self.CACHE[(self.value, depth)] = left.blink(depth+1, maxdepth-1) + right.blink(depth+1, maxdepth-1)
+            return self.CACHE[(self.value, depth)]
 
         # If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-        self.value *= 2024
-        return [self]
+        stone = Stone(self.value * 2024)
+        self.CACHE[(self.value, depth)] = stone.blink(depth+1, maxdepth-1)
+        return self.CACHE[(self.value, depth)]
 
 def parse(my_input: list[str]) -> list[Stone]:
     result: list[Stone] = []
@@ -48,20 +61,22 @@ def print_stones(stones: list[Stone]) -> None:
     print(f'{list(map(str, stones))} - {len(stones)}')
 
 def solution1(my_input: list[str]) -> int:
+    Stone.CACHE = {}
     data = parse(my_input)
-    for i in range(25):
-        # print(len(data))
-        # print_stones(data)
-        stones: list[Stone] = []
-        for stone in data:
-            stones.extend(stone.blink())
-        data = stones
-    # print_stones(data)
-    return len(data)
+    total = 0
+    for stone in data:
+        length = stone.blink(0, 25)
+        total += length
+    return total
 
 def solution2(my_input: list[str]) -> int:
+    Stone.CACHE = {}
     data = parse(my_input)
-    return -1 # TODO
+    total = 0
+    for stone in data:
+        length = stone.blink(0, 75)
+        total += length
+    return total
 
 if __name__ == '__main__':
     for part in PARTS:
--- 11/thoughts.md
+++ 11/thoughts.md
@@ -3,4 +3,16 @@
 * no need for efficiency in part 1
 
 # Part 2
-* 
+* let's see how long it takes to go 3 times bigger
+* uh-oh - even the sample is taking a long time
+* ok, time to start caching things
+    * that didn't help - at least not the way I did the caching
+* looking at the profiler, I see several built-in methods taking ages (due to how often they're called)
+    * I tried optimizing a few of those out (operating on one term at a time, operating only on ints (instead of str conversions), the other way w/ operating only on strs, and using `list.append()` instead of `list.insert()` (this one actually helped somewhat))
+    * I think I need to combine caching with some of those improvements
+* caching idea: dict w/ `tuple[value: int, depth: int]` to `number_of_stones: int`
+* A few things I realized:
+    1. the ordering of the stones does not matter - so I didn't need to do a `list.insert()`, which is slow
+    2. my original caching solution was to cache the initial value and its result (e.g. `{0:1, 1:2024, 2024:[20,24]}`, etc, which really did not help at all - instead I had to use my cache to map the stones value and its "depth" to the number of stones that stone would produce after `depth==maxdepth`, e.g. (for a stone with value 0 and `maxdepth=4`: `{(2, 4): 1, (0, 4): 1, (20, 3): 2, (4, 4): 1, (24, 3): 2, (2024, 2): 4, (1, 1): 4, (0, 0): 4}`
+    3. (I knew this, but always forget) recursion is not always the bottleneck (I was actively avoiding recursion calls, even though I knew it'd be easier to write the code that way, so I could try to flatten the recursion stack)
+* final solution took it from "this will never finish" to 0.02s (CPU time, it was 0.2s user-time)
